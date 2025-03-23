5c1c668e451ca57aa2ed10ce0198b0672724203c
Naomi <naomichoy03@gmail.com>
2025-03-22 22:29:12 -0500

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -15,25 +15,6 @@ class App extends Component {
       </div>
     );
   }
-  /*
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
-      </div>
-    );
-  }*/
 }
 
 export default App;
