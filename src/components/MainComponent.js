471b4237e7cd7b608f1ab64be3390008bcfe80cf
Naomi <naomichoy03@gmail.com>
2025-04-11 20:49:35 -0500

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,12 +7,20 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect, Routes, Navigate } from 'react-router-dom';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
             selectedDish: null
         };
     }
@@ -22,7 +30,11 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             )
         }
 
@@ -48,7 +60,7 @@ class Main extends Component {
                     <Route path='/home' element={<HomePage />} />
                     <Route exact path='/menu' element={<Menu dishes={this.state.dishes} />} />
                     <Route path='/aboutus' element={<div>About Us</div>} />
-                    <Route path='/contactus' element={<div>Contact Us</div>} />
+                    <Route exact path='/contactus' element={<Contact />} />
                     <Route path="/*" element={<HomePage />} />
                 </Routes>
                 <Footer />
