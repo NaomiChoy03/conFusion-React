63c3e63e2087bd23ab73d72ce16254d39a0ed190
Naomi <naomichoy03@gmail.com>
2025-04-25 19:56:50 -0500

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,22 @@
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
+    );
+    return store;
+}
+
+/*import {createStore} from 'redux';
 import { Reducer, initialState } from './reducer'
 
 export const ConfigureStore = () => {
@@ -9,3 +27,4 @@ export const ConfigureStore = () => {
 
     return store;
 }
+*/
